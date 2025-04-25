74c216ca2188959a34f4b0d2279c9d20f519436d
Alexander Mishtenkov <oneofall@gmail.com>
2025-04-25 13:37:16 +0300

    days and weeks added in the added part of the card

--- main.js
+++ main.js
@@ -200,13 +200,19 @@ function formatTimeAgo(timestamp) {
     const seconds = Math.floor(diffMs / 1000);
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
 
     if(seconds < 60) {
         return `${seconds} sec${seconds !== 1 ? 's':''} ago`;
     } else if(minutes < 60) {
         return `${minutes} min${minutes !== 1 ? 's':''} ago`;
-    } else  {
+    } else if(hours < 24) {
         return `${hours} hr${hours !== 1 ? 's' : ''} ago`;
+    } else if(days < 7){
+        return `${days} day${days !== 1 ? 's' : ''} ago`;
+    } else {
+        return `${weeks} week${weeks !== 1 ? 's': ''} ago`;
     }
 }
 
